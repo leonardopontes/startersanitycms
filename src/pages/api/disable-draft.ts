8522f237dcb7c70bee8d85ac8f08907e2c049d75
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-28 18:11:38 -0300

    Update disable-draft.ts

--- src/pages/api/disable-draft.ts
+++ src/pages/api/disable-draft.ts
@@ -1,10 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-
+// desabilitar(
 export default function disable(
+  // requisição: SolicitarNextApi,
   req: NextApiRequest,
+  // resposta: RespostaNextApi<string | void>,
   res: NextApiResponse<string | void>,
 ) {
+  // responder.definindoModoRascunho({ habilitar: falso })
   res.setDraftMode({ enable: false })
+  // responder.escrevendoCabeça(307, { Localização: '/'})
   res.writeHead(307, { Location: '/' })
+  // responder.finalizando()
   res.end()
 }
