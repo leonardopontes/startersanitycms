4b581a6b674e65cd00217313730cfb06130a0453
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-28 13:13:36 -0300

    Update sanity.links.ts

--- src/lib/sanity.links.ts
+++ src/lib/sanity.links.ts
@@ -1,16 +1,29 @@
 export function resolveHref(
+  // Tipodocumento?: string,
   documentType?: string,
+  // slug?: string,
   slug?: string,
+// string ou indefinido {  
 ): string | undefined {
+  // Se (Tipodocumento) { for verdade
   switch (documentType) {
+    // caso 'home':  
     case 'home':
+      // retornar '/'
       return '/'
+    // caso 'página':  
     case 'page':
+      // retornar slug ? `/${slug}` : indefinido
       return slug ? `/${slug}` : undefined
+    // caso 'projeto':  
     case 'project':
+      // retornar slug ? `/projects/${slug}` : indefinido
       return slug ? `/projects/${slug}` : undefined
+    // padrão:   
     default:
+      // aviso.console('tipo de documento Inválido:', Tipodocumento)
       console.warn('Invalid document type:', documentType)
+      // retornar indefinido
       return undefined
   }
 }
