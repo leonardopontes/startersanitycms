c7ea27ca15380ef4b30d0267e62e22b537089ec1
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-28 12:54:47 -0300

    Update sanity.image.ts

--- src/lib/sanity.image.ts
+++ src/lib/sanity.image.ts
@@ -2,17 +2,21 @@ import createImageUrlBuilder from '@sanity/image-url'
 import type { Image } from 'sanity'
 
 import { dataset, projectId } from '~/lib/sanity.api'
-
+// Construirimagem ligando a = criarImagemUrlConstrutora({
 const imageBuilder = createImageUrlBuilder({
+  // Id do projeto: Id do projeto Ou '',
   projectId: projectId || '',
+  // conjunto de dados: conjunto de dados Ou '',
   dataset: dataset || '',
 })
-
+// urlParaImagem ligando a = (fonte: Imagem) contendo => {
 export const urlForImage = (source: Image) => {
   // Ensure that source image contains a valid reference
-  if (!source?.asset?._ref) {
+  // Se (!fonte?.ativa?._referência) { for verdade
+  if (!source?.asset?._ref) {]
+    // retornar indefinido
     return undefined
   }
-
+  // retornar Construirimagem?.imagem(fonte).auto('formato')
   return imageBuilder?.image(source).auto('format')
 }
