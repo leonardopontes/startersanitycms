05837bb68967c737e42a64a8af725ef868563082
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-29 19:07:20 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,27 +8,30 @@ import { readToken } from '~/lib/sanity.api'
 import { getClient } from '~/lib/sanity.client'
 import { getPosts, type Post, postsQuery } from '~/lib/sanity.queries'
 import type { SharedPageProps } from '~/pages/_app'
-
-export const getStaticProps: GetStaticProps<
-  SharedPageProps & {
-    posts: Post[]
-  }
-> = async ({ draftMode = false }) => {
+// pegarPropriedadesEstática: PegarPropriedadesEstática<CompartilharPageProps & { posts: Post[] }> ligando a = função assíncrona ({ Modorascunho = falso }) contendo => {
+export const getStaticProps: GetStaticProps<SharedPageProps & { posts: Post[] }> = async ({ draftMode = false }) => {
+  // client = pegarClient(Modorascunho ? { token: lerToken } : indefinido)
   const client = getClient(draftMode ? { token: readToken } : undefined)
+  // posts = aguardar pegarPosts(client)
   const posts = await getPosts(client)
-
+  // retornar
   return {
+    // propriedades: {
     props: {
+      // Modorascunho,
       draftMode,
+      // token: Modorascunho ? lerToken : '',
       token: draftMode ? readToken : '',
       posts,
     },
   }
 }
-
+// ÍndicePágina(
 export default function IndexPage(
+  // propriedades: InferirPegarTiposdePropriedadesEstáticas<tipode pegarPropriedadesEstáticas>,
   props: InferGetStaticPropsType<typeof getStaticProps>,
 ) {
+  // [posts] = usarConsultaAoVivo<Post[]>(propiedades.posts, Consultarposts)
   const [posts] = useLiveQuery<Post[]>(props.posts, postsQuery)
   return (
     <Container>
